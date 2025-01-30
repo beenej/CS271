d8102fba8eea39f46f206f66bf46b2343ba16092
James B Beene <156967505+beenej@users.noreply.github.com>
2025-01-30 01:13:25 -0800

    Update Assignment2.asm

--- Assignment2.asm
+++ Assignment2.asm
@@ -10,8 +10,7 @@ INCLUDE Irvine32.inc
 
 ; (insert constant definitions here)
 
-UpperLimit EQU 1000				;Upper limit of inputs from user.
-
+UpperLimit EQU 1000  ; Upper limit of inputs from user.
 
 .data
 ; (insert variable definitions here)
@@ -26,23 +25,23 @@ UpperLimit EQU 1000				;Upper limit of inputs from user.
     Begin BYTE "This program will calculate the factors of each number "
           BYTE "within a range of numbers between 1 and 1,000 and indicates which numbers are prime. "
           BYTE "In order to do this, please follow the prompts given. "
-          BYTE "To begin, please enter your name:", 0                 ;begin instructions
-    lowBoundPrompt BYTE "Now, please enter the lower value for the range: ",0 ;prompt for the lower value
-    uppBoundPrompt BYTE "Next, the upper value for the range: ", 0    ; prompt for the upper value
+          BYTE "To begin, please enter your name:", 0  ; begin instructions
+    lowBoundPrompt BYTE "Now, please enter the lower value for the range: ", 0  ; prompt for the lower value
+    uppBoundPrompt BYTE "Next, the upper value for the range: ", 0  ; prompt for the upper value
     continuePrompt BYTE "Enter 1 to do another calculation, or 0 to exit: ", 0
 
 ; --------------------------------------------------------------------------------------------------------
 
     ; messages to user
 
-    inputErrMsg BYTE "Invalid input, please try again. Remember, the value must be between 1 and 1000" ;error message after validation fail
-    listPrimeMsg BYTE " Prime Number", 0 ; number is a prime number
-    listPerfectSquareMsg BYTE " Perfect Square", 0  ;number is a perfect square
+    inputErrMsg BYTE "Invalid input, please try again. Remember, the value must be between 1 and 1000", 0  ; error message after validation fail
+    listPrimeMsg BYTE " Prime Number", 0  ; number is a prime number
+    listPerfectSquareMsg BYTE " Perfect Square", 0  ; number is a perfect square
     exitMsg BYTE "Thanks for using our program!", 0
 
 ; --------------------------------------------------------------------------------------------------------
 
-    username BYTE 50 DUP(0)    ;username entered by user
+    username BYTE 50 DUP(0)  ; username entered by user
     lowBound DWORD ?
     uppBound DWORD ?
     userSelection DWORD ?
@@ -50,162 +49,164 @@ UpperLimit EQU 1000				;Upper limit of inputs from user.
 ; --------------------------------------------------------------------------------------------------------
 
 .code
-; (insert executable instructions here)
+main PROC
 
     ; Print Intro
-    call clrscr
-    mov edx, OFFSET Intro
-    call writestring
-    call crlf
+    CALL Clrscr
+    MOV edx, OFFSET Intro
+    CALL WriteString
+    CALL Crlf
 
     ; Prompt for user name
-    mov edx, OFFSET Begin
-    call writestring
-    call crlf
+    MOV edx, OFFSET Begin
+    CALL WriteString
+    CALL Crlf
 
     ; Store user name
-    mov edx, OFFSET username
-    mov ecx, 50
-    call ReadString
-    call Crlf
+    MOV edx, OFFSET username
+    MOV ecx, 50
+    CALL ReadString
+    CALL Crlf
+
+    MOV BYTE PTR [edx+ecx], 0
 
     ; Prompt for lower bound, loop until valid input
 inputLower:
-    mov edx, OFFSET lowBoundPrompt
-    call writestring
-    call readint
-    mov lowBound, eax
-    cmp eax, 1
-    jl invalidInputs
-    cmp eax, UpperLimit
-    jg invalidInputs
+    MOV edx, OFFSET lowBoundPrompt
+    CALL WriteString
+    CALL ReadInt
+    MOV lowBound, eax
+    CMP eax, 1
+    JL invalidInputs
+    CMP eax, UpperLimit
+    JG invalidInputs
 
     ; Prompt for upper bound, loop until valid input
 inputUpper:
-    mov edx, OFFSET uppBoundPrompt
-    call WriteString
-    call ReadInt
-    mov uppbound, eax
-    cmp eax, 1
-    jl invalidInputs
-    cmp eax, UpperLimit
-    jg invalidInputs
-    cmp eax, lowbound
-    jl invalidInputs
+    MOV edx, OFFSET uppBoundPrompt
+    CALL WriteString
+    CALL ReadInt
+    MOV uppBound, eax
+    CMP eax, 1
+    JL invalidInputs
+    CMP eax, UpperLimit
+    JG invalidInputs
+    CMP eax, lowBound
+    JL invalidInputs
 
     ; Loop through range of numbers
-    mov ecx, lowBound
+    MOV ecx, lowBound
 calculate:
-    cmp ecx, uppBound   ;ensure lower bound is less than upper bound
-    jg endFactor        ;if not, send error message to reprompt inputs
-    
-    call Crlf
-    mov eax, ecx
-    mov edx, OFFSET " : "
-    call WriteString
+    CMP ecx, uppBound  ; ensure lower bound is less than upper bound
+    JG endFactor  ; if not, send error message to reprompt inputs
+
+    CALL Crlf
+    MOV eax, ecx
+    MOV edx, OFFSET " : "
+    CALL WriteString
 
     ; Calculate factors in range
-    mov ebx, 1
+    MOV ebx, 1
 getFactors:
-    cmp ebx, ecx
-    jg detPrime       ;if ebx is greater than ecx, determine if prime or not
+    CMP ebx, ecx
+    JG detPrime  ; if ebx is greater than ecx, determine if prime or not
 
-    mov edx, 0
-    mov eax, ecx
-    div ebx           ;divide upper bound by 1 to get remainder
+    MOV edx, 0
+    MOV eax, ecx
+    DIV ebx  ; divide upper bound by 1 to get remainder
 
-    cmp edx, 0
-    jne notFactor     ;if remainder is not 0, not a factor
-    
+    CMP edx, 0
+    JNE notFactor  ; if remainder is not 0, not a factor
+
     ; if it's a factor, print the factor
-    call WriteInt  ; Print factor
-    mov edx, OFFSET ", "
-    call WriteString
+    CALL WriteInt  ; Print factor
+    MOV edx, OFFSET ", "
+    CALL WriteString
 
 notFactor:
-    inc ebx           ;increment ebx to check next factor
-    jmp getFactors
+    INC ebx  ; increment ebx to check next factor
+    JMP getFactors
 
-    ;determine if number is prime or not
+    ; determine if number is prime or not
 detPrime:
-    mov eax, ecx
-    cmp eax, 1
-    jle notPrime
+    MOV eax, ecx
+    CMP eax, 1
+    JLE notPrime
 
-    mov ebx, 2
+    MOV ebx, 2
 getPrime:
-    cmp ebx, eax
-    jge isPrime
-    
-    mov edx, 0
-    div ebx          ;divide upper bound by 2 to get remainder
+    CMP ebx, eax
+    JGE isPrime
 
-    cmp edx, 0
-    je notPrime      ;if remainder is 0, it's not a prime number
-    inc ebx          ;increment ebx to check next factor
-    jmp getPrime     ;loop back to getPrime
+    MOV edx, 0
+    DIV ebx  ; divide upper bound by 2 to get remainder
+
+    CMP edx, 0
+    JE notPrime  ; if remainder is 0, it's not a prime number
+    INC ebx  ; increment ebx to check next factor
+    JMP getPrime  ; loop back to getPrime
 
 isPrime:
-    mov edx, OFFSET listPrimeMsg
-    call WriteString
-    jmp detPerfectSquare
+    MOV edx, OFFSET listPrimeMsg
+    CALL WriteString
+    JMP detPerfectSquare
 
 notPrime:
-    jmp detPerfectSquare
+    JMP detPerfectSquare
 
 detPerfectSquare:
-    mov eax, ecx
-    mov ebx, 1
+    MOV eax, ecx
+    MOV ebx, 1
 
 perfectSquare:
-    mov edx, 0
-    mul ebx               ; EAX = EBX * EBX (square EBX)
-    cmp eax, ecx          ; Compare with the original number
-    je isPerfectSquare    ; If they are equal, it's a perfect square
-    ja notPerfectSquare   ; If EBX^2 > ECX, exit (not a square)
-    inc ebx               ; Increment EBX (next number to square)
-    jmp perfectSquare ; Repeat for the next number
+    MOV edx, 0
+    MUL ebx  
+    CMP eax, ecx  
+    JE isPerfectSquare
+    
+    JA notPerfectSquare  
+    INC ebx  
+    JMP perfectSquare  
 
 isPerfectSquare:
-    mov edx, OFFSET listPerfectSquareMsg
-    call WriteString
-    jmp endPerfectSquare    ; perfect square process is done
+    MOV edx, OFFSET listPerfectSquareMsg
+    CALL WriteString
+    JMP endLoopSteps  ; perfect square process is done
 
 notPerfectSquare:
-    jmp endLoopSteps
+    JMP endLoopSteps
 
 endLoopSteps:
-    call Crlf
-    inc ecx
-    jmp calculate
+    CALL Crlf
+    INC ecx
+    JMP calculate
 
 endFactor:
     ; Prompt user to continue or exit
 continueOrExit:
-    mov edx, OFFSET continuePrompt
-    call WriteString
-    call ReadInt
-    mov userSelection, eax
-    cmp eax, 0
-    je exitProgram
-    cmp eax, 1
-    je inputLower
-    jmp continueOrExit
+    MOV edx, OFFSET continuePrompt
+    CALL WriteString
+    CALL ReadInt
+    MOV userSelection, eax
+    CMP eax, 0
+    JE exitProgram
+
+    CMP eax, 1
+    JE inputLower
+    JMP continueOrExit
 
 exitProgram:
-    mov edx, OFFSET exitMsg     
-    call WriteString
+    MOV edx, OFFSET exitMsg
+    CALL WriteString
 
-	exit	; exit to operating system
+    EXIT  ; exit to operating system
 
 invalidInputs:
-    MOV edx, OFFSET invalidInput
+    MOV edx, OFFSET inputErrMsg
     CALL WriteString
     CALL Crlf
     JMP inputLower
 
 main ENDP
-
-; (insert additional procedures here)
 
 END main
